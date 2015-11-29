9b4d98321218143f4a50d892a9bff73f5d63433f
Markus Möslinger <markus.moeslinger@gmail.com>
2015-11-29 20:21:37 +0000

    first commit

---
+++ src/main/java/org/outofrange/CrowdsupportApplication.java
@@ -0,0 +1,12 @@
+package org.outofrange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdsupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdsupportApplication.class, args);
+    }
+}
