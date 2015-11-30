bfd6d107526785af7f5fa11cfacf7832afbcf20a
Markus Möslinger <markus.moeslinger@gmail.com>
2015-11-30 08:37:48 +0000

    - database config done via application.properties

---
+++ src/main/java/org/outofrange/crowdsupport/config/DatabaseConfig.java
@@ -0,0 +1,45 @@
+package org.outofrange.crowdsupport.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "database")
+public class DatabaseConfig {
+    private String driver;
+    private String url;
+    private String username;
+    private String password;
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/main/java/org/outofrange/crowdsupport/spring/SpringConfiguration.java
+++ src/main/java/org/outofrange/crowdsupport/spring/SpringConfiguration.java
@@ -1,5 +1,6 @@
 package org.outofrange.crowdsupport.spring;
 
+import org.outofrange.crowdsupport.config.DatabaseConfig;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,10 +8,14 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.inject.Inject;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfiguration {
+    @Inject
+    private DatabaseConfig databaseConfig;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -18,8 +23,8 @@ public class SpringConfiguration {
 
     @Bean
     public DataSource dataSource() {
-        return new DataSourceBuilder(getClass().getClassLoader()).driverClassName("org.h2.Driver")
-                .url("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
-                .username("sa").password("").build();
+        return new DataSourceBuilder(getClass().getClassLoader()).driverClassName(databaseConfig.getDriver())
+                .url(databaseConfig.getUrl())
+                .username(databaseConfig.getUsername()).password(databaseConfig.getPassword()).build();
     }
 }
