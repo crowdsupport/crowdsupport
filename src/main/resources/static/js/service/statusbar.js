df56bea39bb2b70ca60d2041a35c1666fd3b500e
Markus Möslinger <markus.moeslinger@gmail.com>
2016-01-17 23:29:43 +0000

    - templating is done in the service

--- src/main/resources/static/js/service/statusbar.js
+++ src/main/resources/static/js/service/statusbar.js
@@ -2,6 +2,30 @@
     angular
         .module("crowdsupport.service.statusbar", [])
         .service("StatusService", function($q, $log) {
+            var defaultTemplate = function(status) {
+                var glyph;
+                var glyphify = function(sign) {
+                    return '<span class="glyphicon glyphicon-' + sign + '" aria-hidden="true"></span> ';
+                };
+
+                switch (status.type) {
+                    case "SUCCESS":
+                        glyph = glyphify("ok-sign");
+                        break;
+                    case "INFO":
+                        glyph = glyphify("info-sign");
+                        break;
+                    case "ERROR":
+                        glyph = glyphify("remove-sign");
+                        break;
+                    default:
+                        glyph = "";
+                        break;
+                }
+
+                return '<div class="message">' + glyph + status.message + '</div>';
+            };
+
             var pastStatus = [];
             var currentStatus = {};
 
@@ -12,8 +36,9 @@
             this.newStatus = function(newStatus) {
                 $log.debug("New status: " + JSON.stringify(newStatus));
 
-                pastStatus.push(currentStatus);
+                newStatus.defaultHtml = defaultTemplate(newStatus);
 
+                pastStatus.push(currentStatus);
                 currentStatus = newStatus;
                 deferred.notify(newStatus);
             };
@@ -22,33 +47,9 @@
             return {
                 restrict: 'C',
                 controller: function(StatusService) {
-                    var statusTemplate = function(status) {
-                        var glyph;
-                        var glyphify = function(sign) {
-                            return '<span class="glyphicon glyphicon-' + sign + '" aria-hidden="true"></span> ';
-                        };
-
-                        switch (status.type) {
-                            case "SUCCESS":
-                                glyph = glyphify("ok-sign");
-                                break;
-                            case "INFO":
-                                glyph = glyphify("info-sign");
-                                break;
-                            case "ERROR":
-                                glyph = glyphify("remove-sign");
-                                break;
-                            default:
-                                glyph = "";
-                                break;
-                        }
-
-                        return '<div class="message">' + glyph + status.message + '</div>';
-                    };
-
                     StatusService.statusPromise.then(null, null, function(newStatus) {
                         $('.statusbar .message').last().addClass('old');
-                        $('.statusbar').append(statusTemplate(newStatus));
+                        $('.statusbar').append(newStatus.defaultHtml);
                     });
                 }
             }
