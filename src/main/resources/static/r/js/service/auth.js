85acbe437be75f552af275c9022a7c897ba7eded
Markus Möslinger <markus.moeslinger@gmail.com>
2016-01-18 06:17:29 +0000

    - only show user menu when logged in - moved scripts to head

--- src/main/resources/static/r/js/service/auth.js
+++ src/main/resources/static/r/js/service/auth.js
@@ -1,40 +1,43 @@
-(function() {
+(function () {
     angular
         .module('crowdsupport.service.auth', ['crowdsupport.service.rest', 'angular-jwt'])
-        .service('Auth', function(Rest, $rootScope, $log, jwtHelper) {
-            $rootScope.user = null;
+        .service('Auth', function (Rest, $rootScope, $log, jwtHelper) {
+            var retrieveUser = function () {
+                $log.debug("Retrieving user...");
+                return Rest.User.get({}, function () {
+                    $rootScope.auth = true;
+                    $log.debug("...retrieved user");
+                });
+            };
+
+            var initUser = function () {
+                var token = localStorage.getItem('token');
+                var user = null;
+                if (token !== null && !jwtHelper.isTokenExpired(token)) {
+                    return retrieveUser();
+                }
+
+                return user;
+            };
+
+            $rootScope.user = initUser();
             $rootScope.auth = false;
 
             this.logout = function () {
                 localStorage.removeItem("token");
+                $rootScope.user = null;
                 $rootScope.auth = false;
             };
 
             this.login = function (username, password) {
                 $log.debug("Logging in...");
 
-                Rest.User.login({username: username, password: password}, function(response) {
+                Rest.User.login({username: username, password: password}, function (response) {
                     localStorage.setItem("token", response.data);
 
                     $log.debug("...logged in");
-                    retrieveUser();
+                    $rootScope.user = retrieveUser();
                 });
             };
-
-            var retrieveUser = function() {
-                $log.debug("Retrieving user...");
-                $rootScope.user = Rest.User.get({}, function() {
-                    $rootScope.auth = true;
-                    $log.debug("...retrieved user");
-                    $log.debug($rootScope.user);
-                });
-            };
-
-            (function() {
-                var token = localStorage.getItem('token');
-                if (token !== null && !jwtHelper.isTokenExpired(token)) {
-                    retrieveUser();
-                }
-            })();
         });
 })();
