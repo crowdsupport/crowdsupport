fa12cd5921742ce7c21f6286a269cbe1f8c85284
Markus Möslinger <markus.moeslinger@gmail.com>
2016-01-31 01:52:16 +0000

    - change timeago to work with uib.tooltip

--- src/main/resources/static/r/js/widget/timeago.js
+++ src/main/resources/static/r/js/widget/timeago.js
@@ -5,18 +5,49 @@
             return {
                 restrict: 'C',
                 scope: {
-                    title: '@'
+                    title: '@',
+                    uibTooltip: '@',
+                    datetime: '@'
                 },
                 link: function (scope, element, attrs) {
                     var parsedDate = $q.defer();
                     parsedDate.promise.then(function () {
                         jQuery(element).timeago();
+
+                        if ($(element).attr('uib-tooltip')) {
+                            element[0].removeAttribute('title');
+                        }
                     });
 
-                    attrs.$observe('title', function (newValue) {
-                        parsedDate.resolve(newValue);
-                    });
+                    var a = null;
+                    if (scope.title) {
+                        a = 'title'
+                    } else if (scope.datetime) {
+                        a = 'datetime';
+                    } else if (scope.uibTooltip) {
+                        a = 'uibTooltip';
+
+                        if (element[0].nodeName === 'TIME') {
+                            attrs.$set('datetime', scope.uibTooltip);
+                        } else {
+                            attrs.$set('title', scope.uibTooltip);
+                        }
+                    }
+
+                    if (a) {
+                        attrs.$observe(a, function () {
+                            parsedDate.resolve();
+                        });
+                    }
                 }
             };
+        })
+        .filter('time', function () {
+            return function (input) {
+                var d = new Date(input);
+
+                return d.getDate() + '.' + _.padLeft(d.getMonth() + 1, 2, '0') + '.' + d.getFullYear() + ' '
+                    + d.getHours() + ':' + d.getMinutes();
+            }
         });
 })();
