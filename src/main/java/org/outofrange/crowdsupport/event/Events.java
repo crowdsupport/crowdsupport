664b3cab55a9d1cfd7dd0d37c6a3357447252b5b
Markus Möslinger <markus.moeslinger@gmail.com>
2016-02-03 01:25:58 +0000

    Events only returns Events

--- src/main/java/org/outofrange/crowdsupport/event/Events.java
+++ src/main/java/org/outofrange/crowdsupport/event/Events.java
@@ -5,32 +5,32 @@ import org.outofrange.crowdsupport.model.*;
 public class Events {
     private Events() { /* no instantiation */ }
 
-    public static EmptyClientEvent indexRequested() {
+    public static Event indexRequested() {
         return new EmptyClientEvent("indexRequested");
     }
 
-    public static ClientEntityChangeEvent<State> state(ChangeType type, State state) {
+    public static Event state(ChangeType type, State state) {
         return new ClientEntityChangeEvent<>(type, state, "state");
     }
 
-    public static ClientEntityChangeEvent<City> city(ChangeType type, City city) {
+    public static Event city(ChangeType type, City city) {
         return new ClientEntityChangeEvent<>(type, city, "city");
     }
 
-    public static ClientEntityChangeEvent<Place> place(ChangeType type, Place place) {
+    public static Event place(ChangeType type, Place place) {
         return new ClientEntityChangeEvent<>(type, place, "place");
     }
 
-    public static ClientEntityChangeEvent<User> user(ChangeType type, User user) {
+    public static Event user(ChangeType type, User user) {
         return new ClientEntityChangeEvent<>(type, user, "user");
     }
 
-    public static ClientEntityChangeEvent<Comment> comment(ChangeType type, Comment comment) {
+    public static Event comment(ChangeType type, Comment comment) {
         return new ClientEntityChangeEvent<>(type, comment,
                 "comment", buildPlaceTopic(comment.getDonationRequest().getPlace()));
     }
 
-    public static ClientEntityChangeEvent<DonationRequest> donationRequest(ChangeType type, DonationRequest donationRequest) {
+    public static Event donationRequest(ChangeType type, DonationRequest donationRequest) {
         return new ClientEntityChangeEvent<>(type, donationRequest,
                 "donationRequest", buildPlaceTopic(donationRequest.getPlace()));
     }
