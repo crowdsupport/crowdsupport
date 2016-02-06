304d10de95eb0d07d76b309794b384f8e17bb999
Markus Möslinger <markus.moeslinger@gmail.com>
2016-02-06 05:01:20 +0000

    - added tests for Statistics

--- src/test/java/org/outofrange/crowdsupport/service/impl/StatisticsServiceImplTest.java
+++ src/test/java/org/outofrange/crowdsupport/service/impl/StatisticsServiceImplTest.java
@@ -1,6 +1,8 @@
 package org.outofrange.crowdsupport.service.impl;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.outofrange.crowdsupport.dto.StatisticsDto;
 import org.outofrange.crowdsupport.persistence.*;
 
 import static org.junit.Assert.*;
@@ -16,6 +18,7 @@ public class StatisticsServiceImplTest {
 
     private StatisticsServiceImpl statisticsService;
 
+    @Before
     public void prepare() {
         stateRepository = mock(StateRepository.class);
         cityRepository = mock(CityRepository.class);
@@ -29,11 +32,39 @@ public class StatisticsServiceImplTest {
 
     @Test
     public void getStatisticsWithResults() {
-        throw new AssertionError("Not yet implemented");
+        when(stateRepository.count()).thenReturn(1L);
+        when(cityRepository.count()).thenReturn(2L);
+        when(placeRepository.count()).thenReturn(3L);
+        when(donationRequestRepository.countByResolved(false)).thenReturn(4L);
+        when(donationRequestRepository.countByResolved(true)).thenReturn(5L);
+        when(userRepository.count()).thenReturn(6L);
+
+        final StatisticsDto dto = statisticsService.getStatistics();
+
+        assertThat(1L, is(equalTo(dto.getStates())));
+        assertThat(2L, is(equalTo(dto.getCities())));
+        assertThat(3L, is(equalTo(dto.getPlaces())));
+        assertThat(4L, is(equalTo(dto.getOpenRequests())));
+        assertThat(5L, is(equalTo(dto.getClosedRequests())));
+        assertThat(6L, is(equalTo(dto.getTotalUsers())));
     }
 
     @Test
     public void getStatisticsWithEverything0() {
-        throw new AssertionError("Not yet implemented");
+        when(stateRepository.count()).thenReturn(0L);
+        when(cityRepository.count()).thenReturn(0L);
+        when(placeRepository.count()).thenReturn(0L);
+        when(donationRequestRepository.countByResolved(false)).thenReturn(0L);
+        when(donationRequestRepository.countByResolved(true)).thenReturn(0L);
+        when(userRepository.count()).thenReturn(0L);
+
+        final StatisticsDto dto = statisticsService.getStatistics();
+
+        assertThat(0L, is(equalTo(dto.getStates())));
+        assertThat(0L, is(equalTo(dto.getCities())));
+        assertThat(0L, is(equalTo(dto.getPlaces())));
+        assertThat(0L, is(equalTo(dto.getOpenRequests())));
+        assertThat(0L, is(equalTo(dto.getClosedRequests())));
+        assertThat(0L, is(equalTo(dto.getTotalUsers())));
     }
 }
