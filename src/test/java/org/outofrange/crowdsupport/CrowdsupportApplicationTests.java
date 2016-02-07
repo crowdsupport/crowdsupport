f551408124f14527c0258978581da2fd5e407f06
Markus Möslinger <markus.moeslinger@gmail.com>
2016-02-07 00:16:31 +0000

    - fixed application test by using WebIntegrationTest

--- src/test/java/org/outofrange/crowdsupport/CrowdsupportApplicationTests.java
+++ src/test/java/org/outofrange/crowdsupport/CrowdsupportApplicationTests.java
@@ -1,14 +1,16 @@
 package org.outofrange.crowdsupport;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-// @RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = CrowdsupportApplication.class)
-@WebAppConfiguration
+@WebIntegrationTest
 public class CrowdsupportApplicationTests {
-
+    @Test
 	public void contextLoads() {
 	}
-
 }
